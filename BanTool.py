9efe3dfd9a1b31041c261abc2506d348afa20004
Logan Shockley <loganv182@gmail.com>
2026-03-15 19:04:26 -0500

    Updated bans

--- BanTool.py
+++ BanTool.py
@@ -151,7 +151,8 @@ def root():
                   color="black")
         ui.button('NSFW Media', on_click=lambda e: reason_input.set_value("NSFW Media"),
                   color="black")
-        ui.button('Malicious Client Use', on_click=lambda e: reason_input.set_value("Malicious Client Use"), color="black")
+        ui.button('Malicious Client Use', on_click=lambda e: reason_input.set_value("Malicious Client Use"), color="black"),
+        ui.button('Bypassing Profanity Filter', on_click=lambda e: reason_input.set_value("Bypassing Profanity Filter"), color="black")
 
 
 
